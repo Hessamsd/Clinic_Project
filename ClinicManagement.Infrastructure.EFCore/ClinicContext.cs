380195da65e2d1887e7af4784411f64fa0a6eb6a
Hessamsd <hessamsadeghi73@gmail.com>
2024-11-01 20:17:29 +0330

    feat : Implemented ClinicContext

---
+++ ClinicManagement.Infrastructure.EFCore/ClinicContext.cs
@@ -0,0 +1,27 @@
+﻿using ClinicManagement.Domain.AppointmentAgg;
+using ClinicManagement.Domain.DoctorAgg;
+using ClinicManagement.Domain.DoctorSecretaryAgg;
+using ClinicManagement.Domain.PatientAgg;
+using ClinicManagement.Domain.SecretaryAgg;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicManagement.Infrastructure.EFCore
+{
+    public class ClinicContext : DbContext
+    {
+        public ClinicContext(DbContextOptions<ClinicContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Secretary> Secretaries { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<DoctorSecretary> DoctorSecretaries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
