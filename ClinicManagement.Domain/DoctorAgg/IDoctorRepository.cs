361a56480027fcc421ba449089bd591069701474
Hessamsd <hessamsadeghi73@gmail.com>
2024-11-01 21:05:20 +0330

    feat: Implement IDoctorRepository interface

---
+++ ClinicManagement.Domain/DoctorAgg/IDoctorRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClinicManagement.Application.Contracts.Doctor;
+using Framework.Domain;
+
+namespace ClinicManagement.Domain.DoctorAgg
+{
+    public interface IDoctorRepository : IRepository<int, Doctor>
+    {
+        Doctor GetDetails(int id);                        
+        List<DoctorVM> Search(DoctorSearchModel searchModel); 
+        List<DoctorVM> GetDoctors();
+
+    }
+}
